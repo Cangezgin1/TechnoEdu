a3a315827ff63b1d4721a7c312dd0b6a0ce179c5
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2022-11-21 17:53:26 +0300

    Öğrenci giriş paneli eklendi.

---
+++ ACİ_Kurs/Controllers/ÖgrenciController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACİ_Kurs.Controllers
+{
+    public class ÖgrenciController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
